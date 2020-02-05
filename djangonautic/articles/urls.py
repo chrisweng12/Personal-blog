6b8aff596d4184efc3cfc169798a32dd31a85324
chrisweng12 <chris86112299.flg06g@nctu.edu.tw>
2020-02-05 18:47:47 +0800

    capturing url parameters

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.article_list)
+    url(r'^$',views.article_list),
+    url(r'^(?P<slug>[\w-]+)/$',views.article_details)
 ]
 
