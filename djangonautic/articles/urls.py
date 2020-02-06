aa95ae3f153346f54fbdd997623b0ea5261f2e56
chrisweng12 <chris86112299.flg06g@nctu.edu.tw>
2020-02-06 17:24:13 +0800

    naming for url apps

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
     url(r'^$',views.article_list, name = 'list'),
     url(r'^(?P<slug>[\w-]+)/$',views.article_details, name = 'details')
