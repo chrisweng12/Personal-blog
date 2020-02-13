08215d286f722cfc677eada95a08ec95a507116d
chrisweng12 <chris86112299.flg06g@nctu.edu.tw>
2020-02-13 23:33:01 +0800

    added media upload function

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from django.conf.urls import url, include
 from . import view
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +30,5 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
